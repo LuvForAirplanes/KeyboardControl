476c826cceed19c6f39335444e218ce0451e1f0a
Micah Stauffer <micah@abcrun.com>
2018-06-30 14:28:58 -0400

    Initial commit of code

---
+++ KeyboardControl/KeyboardControl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KeyboardControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyboard = new Keyboard();
+            keyboard.Send(Keyboard.ScanCodeShort.KEY_W);
+            keyboard.Send(Keyboard.ScanCodeShort.KEY_O);
+            keyboard.Send(Keyboard.ScanCodeShort.KEY_R);
+            keyboard.Send(Keyboard.ScanCodeShort.KEY_L);
+            keyboard.Send(Keyboard.ScanCodeShort.KEY_D);
+            Console.ReadLine();
+        }
+    }
+}
